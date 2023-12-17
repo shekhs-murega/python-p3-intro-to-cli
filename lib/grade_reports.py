c9ad83bd2d18ed81c156bf6892ae60863c7aa6ab
shekinah <nyamburamary1212@gmail.com>
2023-12-17 12:07:08 +0300

    Intro to CLI

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
+import os
+
+def create_grade_report(student_grades):
+    os.makedirs('reports', exist_ok=True)  # Ensure 'reports' directory exists
+    file_path = os.path.join('reports', 'grade_report.txt')  # Proper file path
+
+    with open(file_path, 'w') as gr:
+        for grade in student_grades:
+            gr.write(grade + '\n')
+
 if __name__ == '__main__':
-    pass
+    student_grades = []
+
+    grade = input("Student name, grade: ")
+    while grade:
+        student_grades.append(grade)
+        grade = input("Student name, grade: ")
+
+    create_grade_report(student_grades)
